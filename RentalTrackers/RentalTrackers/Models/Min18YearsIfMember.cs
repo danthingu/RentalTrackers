25a7a7c07c2e4051f731710172fa1d04cfe02842
Thien Nguyen <thtnguyen93@gmail.com>
2017-09-18 01:07:05 -0700

    add validation message and highlight and text color to red in Site.css inside Content folder

---
+++ RentalTrackers/RentalTrackers/Models/Min18YearsIfMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RentalTrackers.Models
+{
+    public class Min18YearsIfMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer) validationContext.ObjectInstance;
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            {
+                return ValidationResult.Success;
+            }
+
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthdate is required");
+
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+            return (age >= 18) ? ValidationResult.Success 
+                               : new ValidationResult("Customer is less than 18 years old, need to be 18 to get membership");
+        }
+    }
+}
