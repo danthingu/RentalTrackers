c94d3c6952e0aafd9de472468c516ffdfbbde253
Thien Nguyen <thtnguyen93@gmail.com>
2017-09-24 23:20:55 -0700

    add everything

---
+++ Models/Manga.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentalTrackers.Models
+{
+    public class Manga
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
